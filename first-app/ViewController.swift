eb1786c09acfe6ac1539b4f99484c786555da805
smallnumbers0 <jchoi53@mail.ccsf.edu>
2025-07-26 16:37:23 -0700

    Initial Commit

---
+++ first-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first-app
+//
+//  Created by numbers on 7/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
